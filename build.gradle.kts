bd275166fd3ec6370e2c001ef1f174ccaaea1dbe
sakitibi <sakitibi10nin@icloud.com>
2025-09-11 22:54:33 +0900

    fabricのバージョン上げた

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "config.plugins"
-version = "3.0.0.2"
+version = "3.0.0.3"
 
 repositories {
     maven { url = uri("https://maven.fabricmc.net/") }
@@ -15,7 +15,7 @@ dependencies {
     minecraft("com.mojang:minecraft:1.19.4")
     mappings("net.fabricmc:yarn:1.19.4+build.1:v2") // 明示的にv2つけると安定します
     modImplementation("net.fabricmc:fabric-loader:0.14.21")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.76.0+1.19.4") // 最新のAPIにしてください
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.87.2+1.19.4") // 最新のAPIにしてください
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
 }
 
